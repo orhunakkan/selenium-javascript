a18530d744e6a05efba0fcf0f6611b91eff06e54
orhunakkan <orhunakkan@outlook.com>
2025-03-03 13:24:33 -0600

    Remove Cucumber configuration and related dependencies from project

--- cucumber.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  default: {
-    paths: ['features/**/*.feature'],
-    require: ['features/step_definitions/**/*.js'],
-    format: ['html:cucumber-report.html', 'summary'],
-    publishQuiet: true
-  }
-};
