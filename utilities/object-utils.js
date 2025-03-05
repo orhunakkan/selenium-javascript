e516290f256cdce1cea3fdee8f3aa944ec5482b4
orhunakkan <orhunakkan@outlook.com>
2025-03-05 05:18:58 -0600

    Remove unused cloneDeep utility function from object-utils.js

--- utilities/object-utils.js
+++
@@ -1,8 +0,0 @@
-/**
- * Deep clone an object
- * @param {Object} obj - Object to clone
- * @returns {Object} - Cloned object
- */
-export function cloneDeep(obj) {
-  return JSON.parse(JSON.stringify(obj));
-}
