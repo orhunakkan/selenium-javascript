83c6d6795609e23791fb5188ae17c0f9d41ab9ad
orhunakkan <orhunakkan@outlook.com>
2025-06-18 13:59:14 -0500

    refactor: remove unnecessary tag from smoke suite description

--- tests/e2e/heroku-app-smoke.spec.js
+++ tests/e2e/heroku-app-smoke.spec.js
@@ -2,7 +2,7 @@ import { By } from "selenium-webdriver";
 import { createDriver } from "../../utilities/driver.js";
 import { expect } from "chai";
 
-describe("Heroku App - Smoke Suite @smoke", () => {
+describe("Heroku App - Smoke Suite", () => {
 
     it("should load the homepage and validate title and basic elements", async () => {
         const driver = await createDriver();
