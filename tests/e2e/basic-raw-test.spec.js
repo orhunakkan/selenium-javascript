910698488083cc32fbdcaabe6bec38fd7abf636b
Orhun Akkan <orhunakkan@outlook.com>
2025-08-29 13:02:41 -0500

    refactor: simplify title retrieval in homepage URL validation test

--- tests/e2e/basic-raw-test.spec.js
+++ tests/e2e/basic-raw-test.spec.js
@@ -5,8 +5,7 @@ describe('Homepage URL validation', () => {
     it('should go to homepage and verify its URL', async () => {
         let driver = await new Builder().forBrowser(Browser.CHROME).build();
         await driver.get('https://bonigarcia.dev/selenium-webdriver-java/index.html');
-        const title = await driver.getTitle();
-        expect(title).equal('Hands-On Selenium WebDriver with Java');
+        expect(await driver.getTitle()).equal('Hands-On Selenium WebDriver with Java');
         await driver.quit();
     });
 })
