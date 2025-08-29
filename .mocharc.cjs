7e09b6e0aea93f2d70241a31190505b6c6a204de
orhunakkan <orhunakkan@outlook.com>
2025-08-29 00:37:44 -0500

    chore: fresh start

--- .mocharc.cjs
+++ .mocharc.cjs
@@ -1,14 +1,1 @@
-module.exports = {
-  reporter: 'mochawesome',
-  reporterOptions: {
-    reportDir: 'mochawesome-report',
-    reportFilename: 'mochawesome',
-    overwrite: true,
-    html: true,
-    json: true,
-  },
-  timeout: 30000,
-  spec: ['tests/**/*.spec.js'],
-  recursive: true,
-  exit: true,
-};
+
